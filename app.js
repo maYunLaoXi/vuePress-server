85cf441ecb84c4a395f6e839cd4600277f44156a
maYunLaoXi <593871583@qq.com>
2020-11-10 11:37:06 +0800

    添加路由

--- app.js
+++ app.js
@@ -1,12 +1,17 @@
 const Koa = require('koa')
-const app = new Koa
 const static = require('koa-static')
 const path = require('path')
+const route = require('./routes')
+
+
+const app = new Koa
+const port = 80
 
 app.use(static(
   path.join(__dirname, '../blog')
 ))
+app.use(route.routes())
 
-app.listen(80, () => {
-  console.log('aaaaaaaa')
+app.listen(port, () => {
+  console.log(`local80 successful at ${port}`)
 })
